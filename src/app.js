92395d9b34ecd9f9b5032f72f55e2364dc9b1a44
PriscillaCorreia <primariafc@gmail.com>
2022-07-16 16:31:53 -0300

    Adiciona index.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const mongoose = require('./database/mongooseConnect')
 const criancaRoutes = require('./router/criancaRoutes')
 const padrinhoRoutes = require('./router/padrinhoRoutes')
-//const index = require('./index/router') é para colocar?
+const index = require('./router/index')
 
 const usersRoutes = require('./router/usersRoutes')
 const swaggerUi = require('swagger-ui-express');
@@ -14,14 +14,16 @@ const app = express()
 app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 
+
 app.use(express.json())
 app.use(cors())
 
 mongoose.connect()
 
-app.use(criancaRoutes)
-app.use(padrinhoRoutes)
-app.use(usersRoutes)
+app.use('/',criancaRoutes)
+app.use('/',padrinhoRoutes)
+app.use('/',usersRoutes)
+app.use('/, index')
 
 
 module.exports = app

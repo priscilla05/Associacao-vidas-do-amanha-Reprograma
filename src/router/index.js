4cb0c4408a14bbd503f7c0f40f865c3f02b0c316
PriscillaCorreia <primariafc@gmail.com>
2022-07-23 11:09:08 -0300

    titulo no index

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
  
 router.get("/", function (req, res) {
     res.status(200).send({
-        title: "Reprograma-Projeto-Livre",
+        title: "Associação vidas do Amanhã",
         version: "1.0.0"
     })
 });
